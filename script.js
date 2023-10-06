03619ae23c327087398f8a8499652e3b512c94c9
jflood1 <141092472+jflood1@users.noreply.github.com>
2023-10-05 17:55:55 -0700

    XMart Board Repositories
     This repository contains the source code for a responsive website dedicated to showcasing a smart board product called "XMART." XMART is an innovative electronic whiteboard designed for modern classrooms and business environments. The website is built using HTML, CSS, and Bootstrap, ensuring a seamless user experience across various devices and screen sizes.

---
+++ script.js
@@ -0,0 +1,39 @@
+                    //Index
+                    function showInfo(){
+                        var x=document.getElementById("productInfo");
+                    if(x.style.display=="none"){
+                            // condition 1 where display is none
+                            x.style.display="block";
+                        }else{
+                            // condition 2 where display is block
+                            x.style.display="none";
+                        }
+
+                    }
+
+                    //Feautures
+                    function showInfo(){
+                        var x=document.getElementById("productInfo");
+                    if(x.style.display=="none"){
+                            // condition 1 where display is none
+                            x.style.display="block";
+                        }else{
+                            // condition 2 where display is block
+                            x.style.display="none";
+                        }
+
+                    }
+
+                    //Contact Form
+                    function validateForm(){
+                        var name=document.getElementById("name").value;
+                        var name=document.getElementById("email").value;
+                        if (name==""||email==""){
+                            alert(" Error: Missing Information ")
+                            return false;
+                        }
+
+
+                    }
+
+                    //Order Form
